57ce07b85ba3ba63951976add71d1c8726735b9a
abolfazl <a.b.f.m.98@gmail.com>
2023-06-07 16:21:10 +0430

    ezafe kardan controler

--- src/controler/RPSControler.java
+++ src/controler/RPSControler.java
@@ -1,4 +1,21 @@
 package controler;
 
+import model.entity.RockPaperScissors;
+import model.service.RPSSerivceRead;
+import model.service.RPSService;
+import model.service.RPSServiceWrite;
+
 public class RPSControler {
+    RPSServiceWrite write = new RPSService();
+    RPSSerivceRead read = new RPSService();
+
+
+    public void whoWin(RockPaperScissors rockPaperScissors) {
+        write.whoWin(rockPaperScissors);
+    }
+
+    public void show() {
+        read.show();
+    }
+
 }
