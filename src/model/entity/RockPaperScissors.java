a7a0743435c107819c4cc6326d85f293f764376e
abolfazl <a.b.f.m.98@gmail.com>
2023-06-07 16:21:19 +0430

    ezafe kardan entity

--- src/model/entity/RockPaperScissors.java
+++ src/model/entity/RockPaperScissors.java
@@ -1,5 +1,15 @@
 package model.entity;
 
 public class RockPaperScissors {
-    
+    private int number;
+    private int rate;
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
